a5963401202817911c2612cf0f23550d75320061
Fernando Marques <fernando.marquesfilho@gmail.com>
2021-05-07 19:02:15 -0300

    removido trecho de código indevido

--- Repetição/ler_nota.py
+++ Repetição/ler_nota.py
@@ -5,12 +5,3 @@ while nota1 > 10 or nota1 < 0:
     nota1 = float(input("Qual a nota 1?"))
 
 print("Nota",nota1)
-
-
-tecla = input("")
-if tecla == 'w':
-    pracima
-if tecla == 's':
-    prabaixo
-if tecla == "a":
-    esquerda
