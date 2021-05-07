ae24ac086f5b0ab74a0ba00abc583f9c42ac66cb
Fernando Marques <fernando.marquesfilho@gmail.com>
2021-05-06 22:06:54 -0300

    Ler nota

---
+++ Repetição/ler_nota.py
@@ -0,0 +1,16 @@
+nota1 = float(input("Qual a nota 1?"))
+
+while nota1 > 10 or nota1 < 0:
+    print("A nota deve ser entre 0 e 10")
+    nota1 = float(input("Qual a nota 1?"))
+
+print("Nota",nota1)
+
+
+tecla = input("")
+if tecla == 'w':
+    pracima
+if tecla == 's':
+    prabaixo
+if tecla == "a":
+    esquerda
