71c0b54c71795d6153e90017df89d19999a6ea8f
Fernando Marques <fernando.marquesfilho@gmail.com>
2021-06-04 19:56:41 -0300

    exemplo listas

---
+++ Lista - Vetor - Array/exemplo.py
@@ -0,0 +1,11 @@
+
+valores = []
+for i in range(5):
+    valor = int(input("Digite um valor"))
+    valores.append(valor)
+
+print(valores)
+print("máximo",max(valores))
+print("min",min(valores))
+print("soma",sum(valores))
+print("quantidade",len(valores))
